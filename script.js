70fcfc289179d59b626617e246d8bb94dba65587
Jouinos93 <jouinimariem05@gmail.com>
2023-05-13 22:50:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var hide=document.querySelector(".btn1")
+
+
+function disapear(){
+    hide.remove()
+}
+function add(x){
+    var x=document.querySelector(".p1");
+    x.innerHTML++;
+}
+function add2(y){
+    var y=document.querySelector(".p2");
+    y.innerHTML++;}
+
+
+    function add3(z){
+        var z=document.querySelector(".p3");
+        z.innerHTML++;}
